0547394367452fb6ebda11b6ea53a955a4aa57ab
motemen <motemen@gmail.com>
2012-10-03 02:14:54 +0900

    SoundCloud: use oEmbed API

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -114,22 +114,40 @@ YouTube.prototype = {
 
 // SoundCloud {{{
 // XXX Currently supports only widget URL
+// http://developers.soundcloud.com/docs#playing
+// http://developers.soundcloud.com/docs/api/html5-widget
 function SoundCloud () {
   this.PLAYER_IFRAME_ID = 'player-soundcloud-iframe';
+  this.CLIENT_ID = '98365098cb72a68cf93fda1fcebf48e8';
   this.deferreds = {};
 }
 
 SoundCloud.prototype = {
   handlesUrl: function (url) {
-    return url.match(/^http:\/\/api\.soundcloud\.com\//);
+    return url.match(/^http:\/\/soundcloud\.com\//);
   },
   play: function (url) {
     if (this.deferreds['play']) {
       this.deferreds['play'].reject();
     }
-    this.prepare(url).done(function (player) {
-      console.log('player.load');
-      player.load(url, { auto_play: true });
+    var soundcloud = this;
+    this.prepare(url).done(function () {
+      console.log('embed ' + url);
+      SC.oEmbed(url, { auto_play: true }, function (oEmbed) {
+        var iframe = $(oEmbed.html).appendTo(document.body);
+        if (!iframe.is('iframe')) {
+          console.log('got no iframe', iframe);
+        }
+        var widget = SC.Widget(iframe.get(0));
+        widget.bind(
+          SC.Widget.Events.READY, function () {
+            widget.bind(
+              SC.Widget.Events.FINISH,
+              function () { soundcloud.onPlayerFinished() }
+            );
+          }
+        );
+      });
     });
     return this.deferreds['play'] = $.Deferred();
   },
@@ -138,32 +156,13 @@ SoundCloud.prototype = {
       return this.deferreds['prepare'];
     }
 
-    var soundcloud = this;
-    return this.deferreds['prepare'] = $.getScript(
-      'http://w.soundcloud.com/player/api.js'
-    ).pipe(function () {
-      var widget = SC.Widget(
-        $('<iframe/>', {
-          id: soundcloud.PLAYER_IFRAME_ID,
-          src: 'http://w.soundcloud.com/player/?url=',
-          scrolling: 'no',
-          frameborder: 0,
-          width: '100%',
-          height: 166
-        }).appendTo(document.body).get(0)
-      );
-      widget.bind(
-        SC.Widget.Events.READY, function () {
-          widget.bind(
-            SC.Widget.Events.FINISH,
-            function () { soundcloud.onPlayerFinished() }
-          );
-        }
-      );
-      return widget;
-    });
+    return this.deferreds['prepare'] = $.when(
+      $.getScript('http://connect.soundcloud.com/sdk.js'),
+      $.getScript('http://w.soundcloud.com/player/api.js')
+    );
   },
   onPlayerFinished: function () {
+    console.log('onPlayerFinished');
     this.deferreds['play'].resolve();
   }
 };
